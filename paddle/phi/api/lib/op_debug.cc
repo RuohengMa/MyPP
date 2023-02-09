08d3075a3f3480358beea4819cf10eb6634acbb5
RuohengMa <maruoheng@baidu.com>
2023-02-09 11:47:14 +0000

    add support for backward ops

--- paddle/phi/api/lib/op_debug.cc
+++ paddle/phi/api/lib/op_debug.cc
@@ -6,9 +6,9 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law || agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES || CONDITIONS OF ANY KIND, either express || implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -383,6 +383,350 @@ void OpOutputDebugger::PrintOutput(
     OpOutputDebugger::PrintOutput(*v_t, backend, true);
   } else {
     std::cout << "  paddle::optional<std::vector<Tensor>>: NOT INITIALIZED"
+              << std::endl;
+  }
+}
+
+void OpOutputDebugger::PrintOutput(const phi::DenseTensor *t, Backend backend) {
+  std::cout << "  Tensor: ";
+  if (!t || !t->initialized()) {
+    std::cout << "output is NOT INITIALIZED" << std::endl;
+    return;
+  }
+  if (t->dtype() == DataType::FLOAT32) {
+    if (backend == Backend::CPU) {
+      const float *cpu_res = static_cast<const float *>(t->dy_acc_debug_data());
+      float sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output FLOAT32 " << sum_res << std::endl;
+      return;
+    } else if (backend == Backend::XPU) {
+      float *cpu_res = new float[t->numel()];
+      xpu_memcpy(cpu_res,
+                 t->dy_acc_debug_data(),
+                 sizeof(float) * t->numel(),
+                 XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+      float sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output FLOAT32 " << sum_res << std::endl;
+      free(cpu_res);
+    } else {
+      std::cout << "this tool does not support backends other than CPU and XPU"
+                << std::endl;
+    }
+  } else if (t->dtype() == DataType::INT32) {
+    if (backend == Backend::CPU) {
+      const int32_t *cpu_res =
+          static_cast<const int32_t *>(t->dy_acc_debug_data());
+      int32_t sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output INT32 " << sum_res << std::endl;
+      return;
+    } else if (backend == Backend::XPU) {
+      int32_t *cpu_res = new int32_t[t->numel()];
+      xpu_memcpy(cpu_res,
+                 t->dy_acc_debug_data(),
+                 sizeof(int32_t) * t->numel(),
+                 XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+      int32_t sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output INT32 " << sum_res << std::endl;
+      free(cpu_res);
+    } else {
+      std::cout << "this tool does not support backends other than CPU and XPU"
+                << std::endl;
+    }
+  } else if (t->dtype() == DataType::FLOAT64) {
+    if (backend == Backend::CPU) {
+      const double *cpu_res =
+          static_cast<const double *>(t->dy_acc_debug_data());
+      double sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output FLOAT64 " << sum_res << std::endl;
+      return;
+    } else if (backend == Backend::XPU) {
+      double *cpu_res = new double[t->numel()];
+      xpu_memcpy(cpu_res,
+                 t->dy_acc_debug_data(),
+                 sizeof(double) * t->numel(),
+                 XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+      double sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output FLOAT64 " << sum_res << std::endl;
+      free(cpu_res);
+    } else {
+      std::cout << "this tool does not support backends other than CPU and XPU"
+                << std::endl;
+    }
+  } else if (t->dtype() == DataType::INT64) {
+    if (backend == Backend::CPU) {
+      const int64_t *cpu_res =
+          static_cast<const int64_t *>(t->dy_acc_debug_data());
+      int64_t sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output INT64 " << sum_res << std::endl;
+      return;
+    } else if (backend == Backend::XPU) {
+      int64_t *cpu_res = new int64_t[t->numel()];
+      xpu_memcpy(cpu_res,
+                 t->dy_acc_debug_data(),
+                 sizeof(int64_t) * t->numel(),
+                 XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+      int64_t sum_res = 0;
+      for (int i = 0; i < t->numel(); i++) {
+        sum_res += cpu_res[i];
+      }
+      std::cout << "output INT64 " << sum_res << std::endl;
+      free(cpu_res);
+    } else {
+      std::cout << "this tool does not support backends other than CPU and XPU"
+                << std::endl;
+    }
+  } else if (t->dtype() == DataType::BOOL) {
+    if (backend == Backend::CPU) {
+      const bool *cpu_res = static_cast<const bool *>(t->dy_acc_debug_data());
+      std::string cpu_res_str;
+      if (*cpu_res == 0) {
+        cpu_res_str = "false";
+      } else if (*cpu_res > 0) {
+        cpu_res_str = "true";
+      } else {
+        std::cout << "output BOOL is ILLEGAL" << std::endl;
+        return;
+      }
+      std::cout << "output BOOL " << *cpu_res << std::endl;
+      return;
+    } else if (backend == Backend::XPU) {
+      uint8_t *cpu_res = new uint8_t[t->numel()];
+      xpu_memcpy(cpu_res,
+                 t->dy_acc_debug_data(),
+                 sizeof(uint8_t) * t->numel(),
+                 XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+      std::string cpu_res_str;
+      if (*cpu_res == 0) {
+        cpu_res_str = "false";
+      } else if (*cpu_res > 0) {
+        cpu_res_str = "true";
+      } else {
+        std::cout << "output BOOL is ILLEGAL" << std::endl;
+        free(cpu_res);
+        return;
+      }
+      std::cout << "output BOOL " << cpu_res_str << std::endl;
+      free(cpu_res);
+    } else {
+      std::cout << "this tool does not support backends other than CPU and XPU"
+                << std::endl;
+    }
+  } else {
+    std::cout << "output dtype " << t->dtype() << " is NOT SUPPORTED"
+              << std::endl;
+  }
+}
+
+void OpOutputDebugger::PrintOutput(const std::vector<phi::DenseTensor *> &v_t,
+                                   Backend backend) {
+  std::cout << "  std::vector<Tensor>: ";
+  if (v_t.size() == 0) {
+    std::cout << "output is NOT INITIALIZED" << std::endl;
+    return;
+  }
+  if (v_t[0]->dtype() == DataType::FLOAT32) {
+    for (int i = 0; (unsigned)i < v_t.size(); i++) {
+      auto t = v_t[i];
+      if (!t || !t->initialized()) {
+        std::cout << "output is NOT INITIALIZED" << std::endl;
+        return;
+      }
+      if (backend == Backend::CPU) {
+        const float *cpu_res =
+            static_cast<const float *>(t->dy_acc_debug_data());
+        float sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output FLOAT32 " << sum_res << std::endl;
+        return;
+      } else if (backend == Backend::XPU) {
+        float *cpu_res = new float[t->numel()];
+        xpu_memcpy(cpu_res,
+                   t->dy_acc_debug_data(),
+                   sizeof(float) * t->numel(),
+                   XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+        float sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output FLOAT32 " << sum_res << std::endl;
+        free(cpu_res);
+      } else {
+        std::cout
+            << "this tool does not support backends other than CPU and XPU"
+            << std::endl;
+      }
+    }
+  } else if (v_t[0]->dtype() == DataType::INT32) {
+    for (int i = 0; (unsigned)i < v_t.size(); i++) {
+      auto t = v_t[i];
+      if (!t || !t->initialized()) {
+        std::cout << "output is NOT INITIALIZED" << std::endl;
+        return;
+      }
+      if (backend == Backend::CPU) {
+        const int32_t *cpu_res =
+            static_cast<const int32_t *>(t->dy_acc_debug_data());
+        int32_t sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output INT32 " << sum_res << std::endl;
+        return;
+      } else if (backend == Backend::XPU) {
+        int32_t *cpu_res = new int32_t[t->numel()];
+        xpu_memcpy(cpu_res,
+                   t->dy_acc_debug_data(),
+                   sizeof(int32_t) * t->numel(),
+                   XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+        int32_t sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output INT32 " << sum_res << std::endl;
+        free(cpu_res);
+      } else {
+        std::cout
+            << "this tool does not support backends other than CPU and XPU"
+            << std::endl;
+      }
+    }
+  } else if (v_t[0]->dtype() == DataType::FLOAT64) {
+    for (int i = 0; (unsigned)i < v_t.size(); i++) {
+      auto t = v_t[i];
+      if (!t || !t->initialized()) {
+        std::cout << "output is NOT INITIALIZED" << std::endl;
+        return;
+      }
+      if (backend == Backend::CPU) {
+        const double *cpu_res =
+            static_cast<const double *>(t->dy_acc_debug_data());
+        double sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output FLOAT64 " << sum_res << std::endl;
+        return;
+      } else if (backend == Backend::XPU) {
+        double *cpu_res = new double[t->numel()];
+        xpu_memcpy(cpu_res,
+                   t->dy_acc_debug_data(),
+                   sizeof(double) * t->numel(),
+                   XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+        double sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output FLOAT64 " << sum_res << std::endl;
+        free(cpu_res);
+      } else {
+        std::cout
+            << "this tool does not support backends other than CPU and XPU"
+            << std::endl;
+      }
+    }
+  } else if (v_t[0]->dtype() == DataType::INT64) {
+    for (int i = 0; (unsigned)i < v_t.size(); i++) {
+      auto t = v_t[i];
+      if (!t || !t->initialized()) {
+        std::cout << "output is NOT INITIALIZED" << std::endl;
+        return;
+      }
+      if (backend == Backend::CPU) {
+        const int64_t *cpu_res =
+            static_cast<const int64_t *>(t->dy_acc_debug_data());
+        int64_t sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output INT64 " << sum_res << std::endl;
+        return;
+      } else if (backend == Backend::XPU) {
+        int64_t *cpu_res = new int64_t[t->numel()];
+        xpu_memcpy(cpu_res,
+                   t->dy_acc_debug_data(),
+                   sizeof(int64_t) * t->numel(),
+                   XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+        int64_t sum_res = 0;
+        for (int i = 0; i < t->numel(); i++) {
+          sum_res += cpu_res[i];
+        }
+        std::cout << "output INT64 " << sum_res << std::endl;
+        free(cpu_res);
+      } else {
+        std::cout
+            << "this tool does not support backends other than CPU and XPU"
+            << std::endl;
+      }
+    }
+  } else if (v_t[0]->dtype() == DataType::BOOL) {
+    for (int i = 0; (unsigned)i < v_t.size(); i++) {
+      auto t = v_t[i];
+      if (!t || !t->initialized()) {
+        std::cout << "output is NOT INITIALIZED" << std::endl;
+        return;
+      }
+      if (backend == Backend::CPU) {
+        const bool *cpu_res = static_cast<const bool *>(t->dy_acc_debug_data());
+        std::string cpu_res_str;
+        if (*cpu_res == 0) {
+          cpu_res_str = "false";
+        } else if (*cpu_res > 0) {
+          cpu_res_str = "true";
+        } else {
+          std::cout << "output BOOL is ILLEGAL" << std::endl;
+          return;
+        }
+        std::cout << "output BOOL " << *cpu_res << std::endl;
+        return;
+      } else if (backend == Backend::XPU) {
+        uint8_t *cpu_res = new uint8_t[t->numel()];
+        xpu_memcpy(cpu_res,
+                   t->dy_acc_debug_data(),
+                   sizeof(uint8_t) * t->numel(),
+                   XPUMemcpyKind::XPU_DEVICE_TO_HOST);
+        std::string cpu_res_str;
+        if (*cpu_res == 0) {
+          cpu_res_str = "false";
+        } else if (*cpu_res > 0) {
+          cpu_res_str = "true";
+        } else {
+          std::cout << "output BOOL is ILLEGAL" << std::endl;
+          free(cpu_res);
+          return;
+        }
+        std::cout << "output BOOL " << cpu_res_str << std::endl;
+        free(cpu_res);
+      } else {
+        std::cout
+            << "this tool does not support backends other than CPU and XPU"
+            << std::endl;
+      }
+    }
+  } else {
+    std::cout << "output dtype " << v_t[0]->dtype() << " is NOT SUPPORTED"
               << std::endl;
   }
 }
